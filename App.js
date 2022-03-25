71e363cf07ffda9fa7279dfb6f728c6c70e74c94
bhavyas340 <101468605+bhavyas340@users.noreply.github.com>
2022-03-25 13:18:36 -0500

    Add files via upload

---
+++ App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { ChatEngine } from 'react-chat-engine';
+
+export function App() {
+	return (
+		<ChatEngine
+			height='100vh'
+			userName='jonBh'
+			userSecret='12341234'
+			projectID='09aca959-0a55-4175-bcf6-f39e934753ed'
+		/>
+	);
+}
+
+export default App;
